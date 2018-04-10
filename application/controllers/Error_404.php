a48b988feed93b1b101a4fe40d41d66057c8090f
darmawanagungp <darmawannap@gmail.com>
2018-04-10 23:53:28 +0700

    Download Excel dan Graphic View

--- application/controllers/Error_404.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Error_404 extends CI_Controller{
-
-	function index(){
-		$this->load->view('v_error_404');
-	}
-}
---
+++ teknoinfo/application/views/v_home.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html class="no-js">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Selamat Datang Di Teknologi Informasi ITS</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Darmawan Agung" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="<?php echo base_url().'theme/favicon.ico'?>">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/owl.carousel.css'?>">
+
+	<link href="//fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+	<?php
+            error_reporting(0);
+            function limit_words($string, $word_limit){
+                $words = explode(" ",$string);
+                return implode(" ",array_splice($words,0,$word_limit));
+            }
+
+        ?>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">Teknologi Informasi<span>.</span></a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Profil Lulusan</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Blog</a></li>
+						<li><a href="<?php echo base_url().'gallery'?>">Gallery</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+						<li class="cta"><a href="<?php echo base_url().'portfolio'?>">Get started</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<aside id="fh5co-hero" class="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/its_1.jpg'?>);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Selamat Datang Di Departemen Teknologi Informasi ITS</h2>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/its_2.jpg'?>);">
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Selamat Datang Di Departemen Teknologi Informasi ITS</h2>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/its_3.jpg'?>);">
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Selamat Datang Di Departemen Teknologi Informasi ITS</h2>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div id="fh5co-why-us" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>JALUR MASUK</h2>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/snm.png'?>" class="img-responsive"></span>
+					<h3>SNMPTN</h3>
+					<p>Seleksi Nasional Masuk Perguruan Tinggi Negeri</p>
+					<p><a href="https://smits.its.ac.id/sarjana/#snmptn" class="btn btn-primary btn-outline with-arrow">Apply Now <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/sbm.png'?>" class="img-responsive"></span>
+					<h3>SBMPTN</h3>
+					<p>Seleksi Bersama Masuk Perguruan Tinggi Negeri</p>
+					<p><a href="https://smits.its.ac.id/sarjana/#sbmptn" class="btn btn-primary btn-outline with-arrow">Apply Now  <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/pkm.png'?>" class="img-responsive"></span>
+					<h3>PKM</h3>
+					<p>Program Kemitraan dan Mandiri</p>
+					<p><a href="https://smits.its.ac.id/sarjana/#pkm" class="btn btn-primary btn-outline with-arrow">Apply Now  <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="fh5co-section-with-image">
+
+		<img src="<?php echo base_url().'theme/images/image_1.jpg'?>" alt="" class="img-responsive">
+		<div class="fh5co-box animate-box">
+			<h2>Selalu Terdepan</h2>
+			<p>Kami selalu mempersiapkan yang terbaik untuk masa depan anda.</p>
+		</div>
+
+	</div>
+
+	<div id="fh5co-blog" class="animate-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>ARTIKEL TERBARU</h2>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+			<?php
+				foreach ($post->result_array() as $j) :
+					$post_id=$j['tulisan_id'];
+					$post_judul=$j['tulisan_judul'];
+					$post_isi=$j['tulisan_isi'];
+					$post_author=$j['tulisan_author'];
+					$post_image=$j['tulisan_gambar'];
+					$post_tglpost=$j['tanggal'];
+					$post_slug=$j['tulisan_slug'];
+			?>
+				<div class="col-md-4">
+					<a class="fh5co-entry" href="<?php echo base_url().'artikel/'.$post_slug;?>">
+						<figure>
+						<img src="<?php echo base_url().'assets/images/'.$post_image;?>" alt="" class="img-responsive">
+						</figure>
+						<div class="fh5co-copy">
+							<h3><?php echo $post_judul;?></h3>
+							<span class="fh5co-date"><?php echo $post_tglpost.' | '.$post_author;?></span>
+							<?php echo limit_words($post_isi,20).'...';?>
+						</div>
+					</a>
+				</div>
+				<?php endforeach;?>
+
+				<div class="col-md-12 text-center">
+					<p><a href="<?php echo base_url().'artikel'?>" class="btn btn-primary btn-outline with-arrow">View More <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
